deb925c7713399f8f079c087cc8689ca40f2e529
Odell Purifoy <opurifoy@springhealth.com>
2024-01-19 10:24:19 -0500

    Added suspense, loading state, error page and not found page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,10 +1,15 @@
 import { getMeals } from '@/lib/meals';
 import Link from 'next/link';
+import { Suspense } from 'react';
 import MealsGrid from '../components/meals/meals-grid';
 import classes from './page.module.css';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals}/>
+}
+
+export default function MealsPage() {
 
   return (
     <>
@@ -20,7 +25,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals}/>
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   )
